3174e1774eac3739cd01afd05a8433c36b4c6064
arockia-dew <arockia@dewsoftware.com>
2020-06-30 17:02:45 +0530

    first commit

---
+++ src/test/java/com/example/SB2demo/Sb2demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SB2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb2demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
